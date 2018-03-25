8221305fb86a9914b6af9c17ca10564b37e33cca
Natalie Anselmi <nanselm1@binghamton.edu>
2018-03-25 16:06:42 -0400

    Working on Part B

---
+++ natClassWork.py
@@ -0,0 +1,25 @@
+class Dog:
+    def __init__(self, x, y, z, pose):
+
+    def moveForward(self, x, y...):
+
+    def moveBack(self, x, y...):
+
+    def jumpUp(self, z, pose...):
+
+    def tripOver(self, pose...):
+
+    def catchDisk(self...):
+
+class Disk:
+     def __init__(self, x, y, z, speed, color):
+
+
+class Hurdle:
+     def __init__(self, x, y):
+
+class Timer:
+    def __init__(self, x, y, time):
+
+class Score:
+    def __init__(self, x, y, score):
