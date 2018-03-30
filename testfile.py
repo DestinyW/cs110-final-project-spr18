74109f09f6014652e659f4cd6852cf28bb9fb057
CMaggie96 <35605683+CMaggie96@users.noreply.github.com>
2018-03-30 09:15:49 -0400

    Add files via upload

---
+++ testfile.py
@@ -0,0 +1,22 @@
+from Dog import *
+from Disk import *
+from Hurdle import *
+from Timer import *
+from Score import *
+
+def main():
+    if __name__ == "__main__":
+        dog = Dog("filename", 10, 5, jump)
+        print(dog)
+    if __name__ == "__main__":
+        disk = Disk("filename", 10, 5, 0, red)
+        print(disk)
+    if __name__ == "__main__":
+        hurdle = Hurdle("filename", 10, 5)
+        print(hurdle)
+    if __name__ == "__main__":
+        timer = Timer("filename", 10, 5, 3)
+        print(timer)
+    if __name__ == "__main__":
+        score = Score("filename", 10, 5, 100)
+        print(score)
