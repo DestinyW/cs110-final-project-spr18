251df339317d4bb3dd1708a192667b9203dc942f
CMaggie96 <35605683+CMaggie96@users.noreply.github.com>
2018-03-31 14:36:24 -0400

    Add files via upload

--- testfile.py
+++ testfile.py
@@ -1,22 +1,18 @@
-from Dog import *
-from Disk import *
-from Hurdle import *
-from Timer import *
-from Score import *
+from natClassWork import *
 
 def main():
     if __name__ == "__main__":
-        dog = Dog("filename", 10, 5, jump)
+        dog = Dog(10, 5, jump)
         print(dog)
     if __name__ == "__main__":
-        disk = Disk("filename", 10, 5, 0, red)
+        disk = Disk(10, 5, 0, red)
         print(disk)
     if __name__ == "__main__":
-        hurdle = Hurdle("filename", 10, 5)
+        hurdle = Hurdle(10, 5)
         print(hurdle)
     if __name__ == "__main__":
-        timer = Timer("filename", 10, 5, 3)
+        timer = Timer(10, 5, 3)
         print(timer)
     if __name__ == "__main__":
-        score = Score("filename", 10, 5, 100)
+        score = Score(10, 5, 100)
         print(score)
