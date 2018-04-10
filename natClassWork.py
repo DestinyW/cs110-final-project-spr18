89ef440fe243905d5cd2885db4b35f1c2cdd07b1
Destiny Walcott <dwalcot1@binghamton.edu>
2018-04-10 18:13:08 -0400

    updated file

--- natClassWork.py
+++ natClassWork.py
@@ -1,12 +1,40 @@
-class Dog:
-    def __init__(self, x, y, pose):
-        self.x = x
-        self.y = y
-        self.pose = pose
+import sys
+import pygame
 
-    def moveForward(self, x, y):
+class Dog(pygame.sprite.Sprite):
+    def __init__(self, name):
+        # initialize all the sprite functionality
+        pygame.sprite.Sprite.__init__(self)
+        # create surface object image
+        self.image = pygame.image.load('dog.png').convert()
+        self.image.set_colorkey(COLOR)
+        # get the rectangle for positioning
+        self.rect = self.image.get_rect()
+        # set up sprite location
+        self.rect.center_x = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        # set other attributes
+        self.name = name
+        self.speed_y = 0
 
-    def moveBack(self, x, y):
+    def update(self):
+        self.speed_y = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.speed_y = -5
+        if keys[pygame.K_DOWN]:
+            self.speed_y = 5
+        self.rect.y += self.speed_y
+        if self.rect.top > HEIGHT:
+            self.rect.top = HEIGHT
+        if self.rect.bottom < 0:
+            self.rect.bottom = 0
+
+    def moveUp(self):
+        self.y -= self.speed
+
+    def moveDown(self):
+        self.y += self.speed
 
     def jumpUp(self, x, y, pose):
 
@@ -14,25 +42,43 @@ class Dog:
 
     def catchDisk(self):
 
-class Disk:
-    def __init__(self, x, y, speed, color):
-        self.x = x
-        self.y = y
-        self.speed = speed
-        self.color = color
-              
-class Hurdle:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y  
+class Frisbee(pygame.sprite.Sprite):
+    def __init__(self, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('frisbee.png').convert()
+        self.image.set_colorkey(COLOR)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.speed_x = random.randrange(-3, 3)
 
-class Timer:
+    def update(self):
+        self.rect.x += self.speed_x
+        if self.rect.right > WIDTH + 10:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.x = random.randrange(-3,3)
+
+class Hurdle(pygame.sprite.Sprite):
+    def __init__(self, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('hurdle.png').convert()
+        self.image.set_colorkey(COLOR)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.speed_x = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.x += self.speed_x
+        if self.rect.right > WIDTH + 10:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.speed_x = random.randrange(-3, 3)
+
+class Timer(pygame.sprite.Sprite):
     def __init__(self, x, y, time):
         self.x = x
         self.y = y
         self.time = time
 
-class Score:
+class Score(pygame.sprite.Sprite):
     def __init__(self, x, y, score):
         self.x = x
         self.y = y
