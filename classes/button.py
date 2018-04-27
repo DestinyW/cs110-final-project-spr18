9e3873911840c3b729333ab414f4b4c0f56d4df8
NatalieAnselmi <35673312+NatalieAnselmi@users.noreply.github.com>
2018-04-27 10:48:46 -0400

    Update button.py

--- classes/button.py
+++ classes/button.py
@@ -2,12 +2,14 @@ import pygame
 
 # model
 class Button(pygame.sprite.Sprite):
-    def __init__(self, image_file, x, y, name):
+    def __init__(self, image_file, x, y, name,on_center=False):
         pygame.sprite.Sprite.__init__(self)    # initialize all the sprite functionality
         self.image = pygame.image.load(image_file).convert_alpha()   # create surface object image
         # get the rectangle for positioning
         self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
-        self.rect.x = x
-        self.rect.y = y
+        if on_center:
+            self.rect.center = (x,y)
+        else:
+            self.rect.x = x
+            self.rect.y = y
         self.name = name
