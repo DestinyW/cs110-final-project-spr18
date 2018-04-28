a7d4b0b5134af43f582e45434489a70f93680ed0
Natalie Anselmi <nanselm1@binghamton.edu>
2018-04-28 19:34:37 -0400

    Score added to end screens

--- controller.py
+++ controller.py
@@ -18,7 +18,7 @@ class Controller:
         self.button = pygame.Surface(self.screen.get_size()).convert()
 
         """Load sprites"""
-        self.dog = dog.Dog("Fido", 300, 400, "assets/GSDogRun1.png")
+        self.dog = dog.Dog("Fido", 300, 400, "assets/GSDogRun1.png") 
         #set up the walls
         self.walls = pygame.sprite.Group()
         for i in range(1):
@@ -34,8 +34,8 @@ class Controller:
         self.instructions2 = background.Background("assets/Inst2.png", [0, 0])
         self.gamescreen = background.Background("assets/GameBG.png", [0, 0])
         self.winner = background.Background("assets/WinBG.png", [0, 0])
-        self.loser = background.Background("assets/LoseBG.png", [0, 0])
-
+        self.loser = background.Background("assets/LoseBG.png", [0, 0])  
+        
         #buttons
         #buttons for the main menu
         self.InstructionsMM = button.Button("assets/InstructionsMM.png", 400, 500, "rules",True)
@@ -58,7 +58,7 @@ class Controller:
         x = random.randrange(750, 800)
         y = random.randrange(280, 630)
         frisbee_number = random.randint(0,3)   #calls on one of the 4 frisbee types
-        if(frisbee_number == 0):
+        if(frisbee_number == 0):  #names frisbee according to image called
             name = "Purple"
         elif(frisbee_number == 1):
             name = "Red"
@@ -66,7 +66,7 @@ class Controller:
             name = "Yellow"
         elif(frisbee_number == 3):
             name = "White"
-        new_frisbee = frisbee.Frisbee(name, x, y, frisbee_number)
+                new_frisbee = frisbee.Frisbee(name, x, y, frisbee_number)  #generates frisbee
         self.frisbees.add(new_frisbee)
         return new_frisbee
 
@@ -76,55 +76,57 @@ class Controller:
         """
         x = random.randrange(750, 800)
         y = random.randrange(350, 630)
-        new_wall = wall.Wall("walls", x, y, "assets/Wall.png")
-        self.walls.add(new_wall)
+        new_wall = wall.Wall("walls", x, y, "assets/Wall.png")   #generates wall
+        self.walls.add(new_wall)    #adds wall
         return new_wall
 
     def gameIntro(self):
         """
         Set up game intro screen
         """
-        button_group = pygame.sprite.Group([self.InstructionsMM,self.PlayMM,self.quitMM])
+        button_group = pygame.sprite.Group([self.InstructionsMM,self.PlayMM,self.quitMM]) #buttons
         intro = True
         while intro:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:    #button to  instructions page
+                #set up button events
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:    
                     mouse = pygame.mouse.get_pos()
-                    if self.InstructionsMM.rect.collidepoint(mouse):
+                    if self.InstructionsMM.rect.collidepoint(mouse):     #button to  instructions page
                         retval = self.gameRules1()
                         if retval:
                             return
-                    if self.quitMM.rect.collidepoint(mouse):
+                if self.quitMM.rect.collidepoint(mouse):   #quit game
                         pygame.quit()
                         quit()
-                    if self.PlayMM.rect.collidepoint(mouse):
+                    if self.PlayMM.rect.collidepoint(mouse):       #start game       
                         # return value for gameplay function
                         return
 
             self.screen.fill([255, 255, 255])   #Fill screen with white
             self.screen.blit(self.mainmenu.image, (0,0)) #put BG over white, under other objects.
-            button_group.draw(self.screen)
+            button_group.draw(self.screen)   #Add buttons
             pygame.display.flip()
 
     def gameRules1(self):
         """
         Set up the first instructions page
         """
-        button_group = pygame.sprite.Group([self.menu,self.next])
+        button_group = pygame.sprite.Group([self.menu,self.next])     #buttons
         rules1 = True
         while rules1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:    #button to  instructions page
+                #set up button events
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:    
                     mouse = pygame.mouse.get_pos()
                     if self.menu.rect.collidepoint(mouse):
                         return
-                    if self.next.rect.collidepoint(mouse):
+                    if self.next.rect.collidepoint(mouse):   
                         retval = self.gameRules2()
                         if retval == 0:
                             return False
@@ -159,14 +161,12 @@ class Controller:
             button_group.draw(self.screen)
             pygame.display.flip()
 
-    def gameWon(self):
+    def gameWon(self, score):
         """
         set up the "You Won" Page
         """
-        #myfont = pygame.font.SysFont('Comic Sans MS', 30) #Trying to display score on gameover screen
-        #txt = myfont.render("Score: " + str(score), (0,0,0)) #Not Fully Working
-
         button_group = pygame.sprite.Group([self.EndAgain,self.EndMenu])
+        myfont = pygame.font.SysFont('Comic Sans MS', 40)
         winner = True
         while winner:
             for event in pygame.event.get():
@@ -181,18 +181,17 @@ class Controller:
                         return False
             self.screen.fill([255, 255, 255])   #Fill screen with white
             self.screen.blit(self.winner.image, (0,0)) #put BG over white, under other objects.
+            textsurfaceScore = myfont.render("Score: " + str(score), True, (255,255,255))
+            self.screen.blit(textsurfaceScore,(310, 250))
             button_group.draw(self.screen)
-            #self.screen.blit(txt, (400,400)) #Not Fully Working
             pygame.display.flip()
 
-    def gameLost(self):
+    def gameLost(self, score):
         """
         Set up the "You Lost" Page
         """
-        #myfont = pygame.font.SysFont('Comic Sans MS', 30) #Trying to display score on gameover screen
-        #txt = myfont.render("Score: " + str(score), (0,0,0)) #Not Fully Working
-
         button_group = pygame.sprite.Group([self.EndAgain, self.EndMenu])
+        myfont = pygame.font.SysFont('Comic Sans MS', 40)
         loser = True
         while loser:
             for event in pygame.event.get():
@@ -207,8 +206,9 @@ class Controller:
                         return False
             self.screen.fill([255, 255, 255])   #Fill screen with white
             self.screen.blit(self.loser.image, (0,0)) #put BG over white, under other objects.
+            textsurfaceScore = myfont.render("Score: " + str(score), True, (255,255,255))
+            self.screen.blit(textsurfaceScore,(295, 270))
             button_group.draw(self.screen)
-            #self.screen.blit(txt, (400,400)) #Not Fully Working
             pygame.display.flip()
 
     def mainLoop(self):
@@ -223,14 +223,14 @@ class Controller:
         self.all_sprites = pygame.sprite.Group([self.walls, self.frisbees, self.dog])
         pygame.key.set_repeat(1,50)
         pygame.time.set_timer(pygame.USEREVENT,5000)
-        pygame.time.set_timer(pygame.USEREVENT+1,7000)
+        pygame.time.set_timer(pygame.USEREVENT+1,10000)
         while t.time_remaining() > 0:
             self.screen.fill([255, 255, 255])   #Fill screen with white
             self.screen.blit(self.gamescreen.image, (0,0)) #put BG over white, under other objects
-            textsurfaceTime = myfont.render(str(t.time_remaining()), False, (0,0,0))
-            textsurfaceHealth = myfont.render("Health: " + str(health), False, (0,0,0))
-            textsurfaceScore = myfont.render("Score: " + str(score), False, (0,0,0))
-
+            textsurfaceTime = myfont.render(str(t.time_remaining()), True, (0,0,0))
+            textsurfaceHealth = myfont.render("Health: " + str(health), True, (0,0,0))
+            textsurfaceScore = myfont.render("Score: " + str(score), True, (0,0,0))
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
@@ -248,16 +248,15 @@ class Controller:
                 if event.type == pygame.USEREVENT+1:
                     self.all_sprites.add(self.addWalls())
 
-
             #check for collisions
             #Collide with wall
             trips = pygame.sprite.spritecollide(self.dog, self.walls, True)
             if(trips):
-                if(health>1):
+                if(health>1):  
                     health -= 1
                 elif(health==1):
                     health = 0
-                    return self.gameLost()    #Lose the game if you run out of health
+                    return self.gameLost(score)    #Lose the game if you run out of health
             #collide with frisbee
             catch = pygame.sprite.spritecollide(self.dog, self.frisbees, True)
             for item in catch:
@@ -269,7 +268,7 @@ class Controller:
                     score += 10
                 elif(item.name == "Yellow"):
                     score += 0
-
+            
             self.all_sprites.draw(self.screen)
             self.screen.blit(textsurfaceTime,(0,0))
             self.screen.blit(textsurfaceHealth,(640,0))
@@ -278,7 +277,7 @@ class Controller:
             self.walls.update()
             self.frisbees.update()
             pygame.display.flip()
-        return self.gameWon()
+        return self.gameWon(score)
 
 def main():
     main_window = Controller()
