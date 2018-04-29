d88abb17c1ab34e1232c5b9bbcc479c6a9a5306f
Natalie Anselmi <nanselm1@binghamton.edu>
2018-04-29 10:38:23 -0400

    made cloud class

---
+++ src/cloud.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+
+# model
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self, x, y, image_file):
+        # initialize all the sprite functionality
+        pygame.sprite.Sprite.__init__(self)
+        # create surface object image
+        self.image = pygame.image.load(image_file).convert_alpha()
+        # get the rectangle for positioning
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        # set other attributes
+        self.screen_width, self.screen_height = pygame.display.get_surface().get_size()
+
+    def update(self):
+        self.rect.x -= 2
+
